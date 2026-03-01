ec5f286612d033df27a21849cce5c2018d54e4f9
Martine R Karlsen <martinekar08@gmail.com>
2026-03-01 12:18:57 +0100

    Create Products component with API fetch and 3600 revalidation

---
+++ src/app/features/products/components/Products.tsx
@@ -0,0 +1,26 @@
+import ProductList from "./ProductList";
+import { Product, ApiResponse } from "@/app/interfaces/product";
+
+/**
+ * Products component
+ *
+ * Fetches products from the "Online Shop" API with caching and revalidation
+ * (revalidates the data at most once every 3600 seconds (1 hour)).
+ *
+ * Passes the fetched products to the ProductList component.
+ *
+ * @returns The rendered ProductList component with the fetched products.
+ */
+export default async function Products() {
+  const response = await fetch(`https://v2.api.noroff.dev/online-shop`, {
+    next: { revalidate: 3600 },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  const result: ApiResponse = await response.json();
+  const products: Product[] = result.data;
+
+  return <ProductList products={products} />;
+}
