d4be14fdaa9a86d1adbec5cc6a979f78356131ec
Martine R Karlsen <martinekar08@gmail.com>
2026-03-02 15:34:46 +0100

    Create SingleProductPage to fetch and display a single product
    - Fetches a single product from the "Online Shop" API using the product ID from the route params.
    - Caches the data and revalidate (revalidates the data at most once every 3600 seconds (1 hour).
    - Passes the fetched product to the SinglePageProduct component.

---
+++ src/app/features/products/product/[id]/page.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { ApiSingleProductResponse } from "@/app/interfaces/product";
+import SinglePageProduct from "../../components/SinglePageProuductCard";
+
+/**
+ * SingleProductPage
+ *
+ * Fetches a single product from the "Online Shop" API using the product ID from the route params.
+ * Caches the data and revalidate
+ * (revalidates the data at most once every 3600 seconds (1 hour)).
+ *
+ * Passes the fetched product to the SinglePageProduct component.
+ *
+ * @returns The rendered SinglePageProduct component with the fetched product.
+ */
+export default async function SingleProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id: productId } = await params;
+
+  const response = await fetch(
+    `https://v2.api.noroff.dev/online-shop/${productId}`,
+    {
+      next: { revalidate: 3600 },
+    }
+  );
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      return <p>Could not find product with id: {productId}.</p>;
+    }
+    return <p>Could not load the product. Try again later.</p>;
+  }
+
+  const result: ApiSingleProductResponse = await response.json();
+  const product = result.data;
+
+  return (
+    <div className="min-h-screen w-full flex flex-col pt-20 px-4 xs:px-6 break-all sm:break-normal items-center">
+      <SinglePageProduct product={product} />
+    </div>
+  );
+}
