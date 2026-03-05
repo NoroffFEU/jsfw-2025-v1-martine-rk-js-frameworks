1aef64ac5abb3f80444e553edce5f86e17ea9aea
Martine R Karlsen <martinekar08@gmail.com>
2026-03-05 18:14:45 +0100

    Wrap Singe Product page in try/catch to handle errors

--- src/app/features/products/product/[id]/page.tsx
+++ src/app/features/products/product/[id]/page.tsx
@@ -18,36 +18,40 @@ export default async function SingleProductPage({
 }: {
   params: { id: string };
 }) {
-  const { id: productId } = await params;
+  try {
+    const { id: productId } = await params;
 
-  const response = await fetch(
-    `https://v2.api.noroff.dev/online-shop/${productId}`,
-    {
-      next: { revalidate: 3600 },
-    }
-  );
+    const response = await fetch(
+      `https://v2.api.noroff.dev/online-shop/${productId}`,
+      {
+        next: { revalidate: 3600 },
+      }
+    );
 
-  if (!response.ok) {
-    if (response.status === 404) {
-      return (
-        <div className="flex h-screen items-center justify-center text-red-700">
-          <p>Could not find product with id: {productId}.</p>
-        </div>
-      );
+    if (!response.ok) {
+      if (response.status === 404) {
+        return (
+          <div className="flex h-screen items-center justify-center text-red-700">
+            <p>Could not find product with id: {productId}.</p>
+          </div>
+        );
+      }
+      throw new Error("Failed to fetch product");
     }
+
+    const result: ApiSingleProductResponse = await response.json();
+    const product = result.data;
+
     return (
-      <div className="flex  h-screen items-center justify-center text-red-700">
+      <div className="min-h-screen w-full flex flex-col pt-20 px-4 xs:px-6 break-all sm:break-normal items-center">
+        <SinglePageProduct product={product} />
+      </div>
+    );
+  } catch {
+    return (
+      <div className="flex h-screen items-center justify-center text-red-700">
         <p>Could not load the product. Try again later.</p>
       </div>
     );
   }
-
-  const result: ApiSingleProductResponse = await response.json();
-  const product = result.data;
-
-  return (
-    <div className="min-h-screen w-full flex flex-col pt-20 px-4 xs:px-6 break-all sm:break-normal items-center">
-      <SinglePageProduct product={product} />
-    </div>
-  );
 }
